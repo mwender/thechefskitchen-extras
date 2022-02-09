749eebc4b38db3423e4fd89c3178e93de3ce95f5
Michael Wender <michael@michaelwender.com>
2022-02-09 12:43:43 -0500

    Enqueuing after Elementor JS

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -8,7 +8,7 @@ function enqueue_scripts(){
   if( file_exists( $css_filename ) )
     wp_enqueue_style( 'tka', TCK_PLUGIN_URL . 'lib/' . TCK_CSS_DIR . '/main.css', ['hello-elementor','elementor-frontend'], filemtime( $css_filename ) );
 
-  wp_register_script( 'elementor-tab-enhancers', TCK_PLUGIN_URL . 'lib/js/elementor-tab-enhancers.js', null, filemtime( TCK_PLUGIN_PATH . 'lib/js/elementor-tab-enhancers.js'), true );
+  wp_register_script( 'elementor-tab-enhancers', TCK_PLUGIN_URL . 'lib/js/elementor-tab-enhancers.js', [ 'elementor-frontend-modules', 'elementor-pro-webpack-runtime', 'pro-elements-handlers', 'elementor-frontend', 'elementor-pro-frontend' ], filemtime( TCK_PLUGIN_PATH . 'lib/js/elementor-tab-enhancers.js'), true );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
 
