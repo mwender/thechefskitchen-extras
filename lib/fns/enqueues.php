5592af9d5de2fca3b312f4ddd8c3d46b399bbd6e
Michael Wender <michael@michaelwender.com>
2022-05-06 12:38:31 -0400

    Registering caleandar.js

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -1,7 +1,9 @@
 <?php
-
 namespace tcw\enqueues;
 
+/**
+ * Enqueues required scripts and styles.
+ */
 function enqueue_scripts(){
   // Our custom styles
   $css_filename = TCW_PLUGIN_PATH . 'lib/' . TCW_CSS_DIR . '/main.css';
@@ -9,6 +11,10 @@ function enqueue_scripts(){
     wp_enqueue_style( 'tka', TCW_PLUGIN_URL . 'lib/' . TCW_CSS_DIR . '/main.css', ['hello-elementor','elementor-frontend'], filemtime( $css_filename ) );
 
   wp_register_script( 'elementor-tab-enhancers', TCW_PLUGIN_URL . 'lib/js/elementor-tab-enhancers.js', [ 'elementor-frontend-modules', 'elementor-pro-webpack-runtime', 'pro-elements-handlers', 'elementor-frontend', 'elementor-pro-frontend' ], filemtime( TCW_PLUGIN_PATH . 'lib/js/elementor-tab-enhancers.js'), true );
+
+  $calendar_js = ( TCW_DEV_ENV )? 'caleandar.js' : 'caleandar.min.js' ;
+  wp_register_script( 'caleandar', TCW_PLUGIN_URL . 'lib/js/' . $calendar_js, null, filemtime( TCW_PLUGIN_PATH . 'lib/js/' . $calendar_js), true );
+  wp_register_script( 'caleandar-init', TCW_PLUGIN_URL . 'lib/js/caleandar.init.js', [ 'caleandar' ], filemtime( TCW_PLUGIN_PATH . 'lib/js/caleandar.init.js'), true );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
 
