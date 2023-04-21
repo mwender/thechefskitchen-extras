84d808bb068691ad749206e4a23eddce1e1ebe85
Michael Wender <michael@michaelwender.com>
2023-04-21 15:50:52 -0400

    Updating columns for Event CPTs

--- lib/fns/admin-custom-columns.php
+++ lib/fns/admin-custom-columns.php
@@ -3,6 +3,19 @@
 namespace tcw\customcolumns;
 
 /**
+ * Admin CSS
+ */
+function admin_custom_css(){
+  echo '<style>
+  .post-type-event .column-title{width: 20%;}
+  .post-type-event .column-event_date{width: 20%;}
+  .post-type-event .column-food_trucks{width: 50%;}
+  .post-type-event .column-tags{width: 10%;}
+  </style>';
+}
+add_action( 'admin_head', __NAMESPACE__ . '\\admin_custom_css' );
+
+/**
  * Adds columns to the Event CPT
  *
  * @param      array  $columns  The columns
@@ -12,16 +25,19 @@ namespace tcw\customcolumns;
 function set_event_edit_columns($columns) {
   $columns['event_date']        = __( 'Event Date', 'tcw' );
   $columns['food_trucks'] = __( 'Food Trucks', 'tcw' );
+  $columns['thumbnail'] = __( 'Thumbnail', 'tcw' );
   /*$columns['location']    = __( 'Location', 'tcw' );*/
 
   // Re-order columns
   $columns = [
     'cb'          => $columns['cb'],
+    'thumbnail'   => $columns['thumbnail'],
     'title'       => $columns['title'],
     'event_date'  => $columns['event_date'],
     'food_trucks' => $columns['food_trucks'],
     /*'location'    => $columns['location'],*/
     'tags'        => $columns['tags'],
+    'date'        => $columns['date'],
   ];
   return $columns;
 }
@@ -60,7 +76,7 @@ function custom_post_order($query){
   if( is_admin() ){
     if( isset( $query->query_vars['post_type'] ) && $query->query_vars['post_type'] == 'event' ){
       $post_status = get_query_var( 'post_status' );
-      if( in_array( $post_status, [ 'publish', 'draft' ]) ){
+      if( in_array( $post_status, [ 'publish', 'draft' ] ) ){
         $query->set( 'meta_key', 'start_date' );
       } else {
         $query->set( 'meta_query', [
@@ -108,7 +124,7 @@ function custom_event_column( $column, $post_id ){
       $start_date = new \DateTime( $start_date );
       $end_date = get_post_meta( $post_id, 'end_date', true );
       $end_date = new \DateTime( $end_date );
-        echo $start_date->format( 'l, M d, Y' ) . '<br>' . $start_date->format( 'g' ). '-' . $end_date->format( 'ga' );
+        echo $start_date->format( 'l, M d, Y' ) . ' (' . $start_date->format( 'g' ). '-' . $end_date->format( 'ga' ) . ')';
       break;
 
     case 'food_trucks':
@@ -117,9 +133,11 @@ function custom_event_column( $column, $post_id ){
         $food_trucks = $food_trucks[0];
         $food_truck_list = [];
         foreach( $food_trucks as $food_truck_id ){
-          $food_truck_list[] = get_the_title( $food_truck_id );
+          //$food_truck_list[] = get_the_title( $food_truck_id );
+          $food_truck_list[] = '<img src="' . get_the_post_thumbnail_url( $food_truck_id, $size = 'post-thumbnail' ) . '" style="width: 64px; height: auto;" alt="' . esc_attr( get_the_title( $food_truck_id ) ) . '" />';
         }
-        echo '<ul style="margin: 0;"><li>' . implode( '</li><li>', $food_truck_list ) . '</li></ul>';
+        //echo '<ul style="margin: 0;"><li>' . implode( '</li><li>', $food_truck_list ) . '</li></ul>';
+        echo implode( ' ', $food_truck_list );
       }
       break;
 
@@ -127,6 +145,13 @@ function custom_event_column( $column, $post_id ){
       $location_id = get_post_meta( $post_id, 'location', true );
       if( $location_id && is_numeric( $location_id ) )
         echo get_the_title( $location_id );
+      break;
+
+    case 'thumbnail':
+      //if( has_post_thumbnail( $post_id ) )
+        //the_post_thumbnail('thumbnail', ['style' => 'width: 48px; height: 48px;'] );
+      if( has_post_thumbnail( $post_id ) )
+        the_post_thumbnail( 'thumbnail', [ 'style' => 'width: 64px; height: auto;' ] );
       break;
   }
 }
